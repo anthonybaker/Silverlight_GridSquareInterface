ddad251cb495db0e293bb5abba71e261bda395d8
Anthony Baker <abakerp@gmail.com>
2012-05-09 14:41:31 +0100

    Initial Commit with sharable code

---
+++ GridSquareInterface/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace GridSquareInterface
+{
+	public partial class MainPage : UserControl
+	{
+        private bool inNormalState = true;
+
+		public MainPage()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MainMsg.Text = "Video ended. Playing video again...";
+            MediaElement video = (MediaElement)sender;
+            video.Position = TimeSpan.Zero;
+            video.Play();
+
+            if (inNormalState)
+            {
+                VisualStateManager.GoToState(this, "One", true);
+                inNormalState = false;
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Normal", true);
+                inNormalState = true;
+            }
+        }
+	}
+}
